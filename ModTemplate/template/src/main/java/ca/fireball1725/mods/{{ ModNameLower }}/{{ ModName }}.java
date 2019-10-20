93c343b624a6bdef0af629079d5b7e56d9fef0a7
FireBall1725 <fireball@fireball1725.com>
2019-10-20 07:00:24 -0700

    fix(template): fixed the boilr template

---
+++ ModTemplate/template/src/main/java/ca/fireball1725/mods/{{ ModNameLower }}/{{ ModName }}.java
@@ -0,0 +1,11 @@
+package ca.fireball1725.mods.{{ ModNameLower }};
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("{{ ModName }}")
+public class {{ ModName }} {
+
+  public {{ ModName }}() {
+
+  }
+}
